4ee833b00f12f5a2675d420b7b133da5d1aa7182
Narendra Pathai <narendra.pathai@gmail.com>
2017-05-03 12:05:05 +0530

    Added inline edit feature, with test cases

--- todo-app/src/app/app.component.spec.ts
+++ todo-app/src/app/app.component.spec.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   let component: AppComponent;
+  let wrapper: AppComponentTestWrapper;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -21,6 +22,7 @@ describe('AppComponent', () => {
     this.fixture = TestBed.createComponent(AppComponent);
     this.component = this.fixture.componentInstance;
     this.fixture.detectChanges();
+    this.wrapper = new AppComponentTestWrapper(this.fixture);
   }));
 
   it('should render title in a h1 tag',() => {
@@ -29,7 +31,7 @@ describe('AppComponent', () => {
   });
 
   it('should render todos', () => {
-    let elements: DebugElement[] = this.fixture.debugElement.queryAll(By.css('.todo'));
+    let elements: DebugElement[] = this.wrapper.getTodoSpans();
     expect(elements.length).toBe(3);
     expect(elements[0].nativeElement.innerText).toBe('Todo 1');
     expect(elements[1].nativeElement.innerText).toBe('Todo 2');
@@ -38,68 +40,45 @@ describe('AppComponent', () => {
 
    it('should be able to add a new todo', fakeAsync(() => {
 
-      let compiled = this.fixture.debugElement.nativeElement;
-      let taskInput = compiled.querySelector('#todo-form > input');
+      let taskInput = this.wrapper.getNewTodo();
+      this.wrapper.setNewTodoValue('New Task');
 
-      taskInput.value = 'New Task';
-      taskInput.dispatchEvent(new Event('input'));
+      let form = this.wrapper.getTodoForm();
+      this.wrapper.submitForm(form);
 
-      this.fixture.detectChanges();
-      tick();
-
-      let form = this.fixture.debugElement.query(By.css('#todo-form'));
-      form.triggerEventHandler('submit', null);
-
-      this.fixture.detectChanges();
-      tick();
-
-      let elements: DebugElement[] = this.fixture.debugElement.queryAll(By.css('.todo'));
+      let elements: DebugElement[] = this.wrapper.getTodoSpans();
       expect(elements.length).toBe(4);
       expect(elements[3].nativeElement.innerText).toBe('New Task');
     }));
 
     it('should reset todo input value after form is submitted', fakeAsync(() => {
 
-      let compiled = this.fixture.debugElement.nativeElement;
-      let taskInput = compiled.querySelector('#todo-form > input');
+      this.wrapper.setNewTodoValue('New Todo');
 
-      taskInput.value = 'New Task';
-      taskInput.dispatchEvent(new Event('input'));
+      let form = this.wrapper.getTodoForm();
+      this.wrapper.submitForm(form);
 
-      this.fixture.detectChanges();
-      tick();
-
-      let form = this.fixture.debugElement.query(By.css('#todo-form'));
-      form.triggerEventHandler('submit', null);
-
-      this.fixture.detectChanges();
-      tick();
-
-      expect(taskInput.value).toBe('');
+      expect(this.wrapper.getNewTodo().value).toBe('');
     }));
 
     it('should remove todo when delete button is clicked', fakeAsync(() => {
 
-      let deleteButton = this.fixture.debugElement.queryAll(By.css('.btn-delete'))[1];
-      deleteButton.triggerEventHandler('click', null);
+      let deleteButton = this.wrapper.getDeleteButtons()[1];
 
-      this.fixture.detectChanges();
-      tick();
+      this.wrapper.clickElement(deleteButton);
 
-      let todos: DebugElement[] = this.fixture.debugElement.queryAll(By.css('.todo'));
+      let todos: DebugElement[] = this.wrapper.getTodoSpans();
       expect(todos.length).toBe(2);
       expect(todos[0].nativeElement.innerText).toBe('Todo 1');
       expect(todos[1].nativeElement.innerText).toBe('Todo 3');
     }));
 
     it('should check the done checkbox and mark the task done when clicked', fakeAsync(() => {
-      let deleteButton = this.fixture.debugElement.queryAll(By.css('.btn-done'))[1];
-      deleteButton.triggerEventHandler('click', null);
+      let doneButton = this.wrapper.getDoneButtons()[1];
 
-      this.fixture.detectChanges();
-      tick();
+      this.wrapper.clickElement(doneButton);
 
-      expect(deleteButton.nativeElement.checked).toBe(true);
+      expect(doneButton.nativeElement.checked).toBe(true);
       // This is a redundunt check as we already have this check in unit test.
       // So we can also use spy here and check that the html is calling component correcly
       // So effectively we will be testing the binding between view and logic
@@ -108,48 +87,38 @@ describe('AppComponent', () => {
     }));
 
     it('should apply task done css when task is marked as done', fakeAsync(() => {
-      let deleteButton = this.fixture.debugElement.queryAll(By.css('.btn-done'))[1];
-      deleteButton.triggerEventHandler('click', null);
+      let doneButton = this.wrapper.getDoneButtons()[1];
+      this.wrapper.clickElement(doneButton);
 
-      this.fixture.detectChanges();
-      tick();
+      let todo = this.wrapper.getTodoSpans()[1];
 
-      let todo: DebugElement = this.fixture.debugElement.queryAll(By.css('.todo'))[1];
-      expect(todo.classes['todo-done']).toBeTruthy();
+      expect(this.wrapper.hasDoneClass(todo)).toBeTruthy();
     }));
 
     it('should remove task done css when task is marked as undone', fakeAsync(() => {
-      let deleteButton = this.fixture.debugElement.queryAll(By.css('.btn-done'))[1];
-      deleteButton.triggerEventHandler('click', null);
+      let doneButton = this.wrapper.getDoneButtons()[1];
 
-      this.fixture.detectChanges();
-      tick();
+      this.wrapper.clickElement(doneButton);
 
-      deleteButton.triggerEventHandler('click', null);
+      this.wrapper.clickElement(doneButton);
 
-      this.fixture.detectChanges();
-      tick();
-
-      let todo: DebugElement = this.fixture.debugElement.queryAll(By.css('.todo'))[1];
-      expect(todo.classes['todo-done']).toBeFalsy();
+      let todo: DebugElement = this.wrapper.getTodoSpans()[1];
+      expect(this.wrapper.hasDoneClass(todo)).toBeFalsy();
     }));
 
-   it('should show not input box for inplace edit by default', fakeAsync(() => {
-     let form = this.fixture.debugElement.queryAll(By.css('.edit-todo-form'))[0];
-     expect(form).toBeFalsy();
+   it('should not show input box for inplace edit by default', fakeAsync(() => {
+     let forms = this.wrapper.getEditableTodoForms();
+     expect(forms.length).toBe(0);
    }));
 
-   it('should show hide span for inplace edit when clicked', fakeAsync(() => {
-     let span = this.fixture.debugElement.queryAll(By.css('.todo'))[0];
+   it('should hide span for inplace edit when clicked', fakeAsync(() => {
+     let span = this.wrapper.getTodoSpans()[0];
 
-     span.triggerEventHandler('click', null);
+     this.wrapper.clickElement(span);
 
-     this.fixture.detectChanges();
-     tick();
+     let allSpans = this.wrapper.getTodoSpans();
 
-     let allSpans = this.fixture.debugElement.queryAll(By.css('.todo'));
      expect(allSpans.length).toBe(2);
-
      allSpans.forEach(eachSpan => {
          expect(eachSpan.nativeElement.value).not.toBe('Todo 1');
      });
@@ -157,34 +126,90 @@ describe('AppComponent', () => {
 
     it('should show input box with task name for inplace edit when span is clicked', fakeAsync(() => {
 
-      let span = this.fixture.debugElement.queryAll(By.css('.todo'))[0];
+      let span = this.wrapper.getTodoSpans()[0];
 
-      span.triggerEventHandler('click', null);
+      this.wrapper.clickElement(span);
 
-      this.fixture.detectChanges();
-      tick();
+      let form = this.wrapper.getEditableTodoForms()[0];
 
-      let form = this.fixture.debugElement.queryAll(By.css('.edit-todo-form'))[0];
-      expect(form.query(By.css('.input-todo')).nativeElement.value).toBe('Todo 1');
+      expect(this.wrapper.getInputTodo(form).nativeElement.value).toBe('Todo 1');
     }));
 
     it('should hide inplace edit input box and show span with task name input box loses focus/blurs', fakeAsync(() => {
+      let span = this.wrapper.getTodoSpans()[0];
 
-      let span = this.fixture.debugElement.queryAll(By.css('.todo'))[0];
+      this.wrapper.clickElement(span);
 
-      span.triggerEventHandler('click', null);
+      let form = this.wrapper.getEditableTodoForms()[0];
 
-      this.fixture.detectChanges();
-      tick();
+      this.wrapper.blurEditableTodoInput(form);
 
-      let form = this.fixture.debugElement.queryAll(By.css('.edit-todo-form'))[0];
-
-      form.query(By.css('.input-todo')).nativeElement.focus = true;
-
-      form.query(By.css('.input-todo')).triggerEventHandler('blur', null);
-      this.fixture.detectChanges();
-      tick();
-
-      expect(this.fixture.debugElement.queryAll(By.css('.todo'))[0].nativeElement.innerText).toBe('Todo 1');
+      expect(this.wrapper.getTodoSpans()[0].nativeElement.innerText).toBe('Todo 1');
     }));
+
+    class AppComponentTestWrapper {
+      constructor(private fixture:  ComponentFixture<AppComponent>) {
+      }
+
+      getTodoSpans(): DebugElement[] {
+          return this.fixture.debugElement.queryAll(By.css('.todo'));
+      }
+
+      clickElement(button: DebugElement) {
+        button.triggerEventHandler('click', null);
+        this.fixture.detectChanges();
+        tick();
+      }
+
+      blurEditableTodoInput(form: DebugElement) {
+        form.query(By.css('.input-todo')).triggerEventHandler('blur', null);
+        this.fixture.detectChanges();
+        tick();
+      }
+
+      getEditableTodoForms() : DebugElement[] {
+        return this.fixture.debugElement.queryAll(By.css('.edit-todo-form'));
+      }
+
+      getInputTodo(form : DebugElement) : DebugElement {
+        return form.query(By.css('.input-todo'));
+      }
+
+      getDoneButtons() : DebugElement[] {
+        return this.fixture.debugElement.queryAll(By.css('.btn-done'));
+      }
+
+      hasDoneClass(todo : DebugElement) : boolean {
+        return todo.classes['todo-done'];
+      }
+
+      getDeleteButtons() : DebugElement[] {
+        return this.fixture.debugElement.queryAll(By.css('.btn-delete'));
+      }
+
+      getNewTodo() : any {
+        let compiled = this.fixture.debugElement.nativeElement;
+        return compiled.querySelector('#todo-form > input');
+      }
+
+      setNewTodoValue(value : string) {
+        let taskInput = this.getNewTodo();
+        taskInput.value = value;
+        taskInput.dispatchEvent(new Event('input'));
+
+        this.fixture.detectChanges();
+        tick();
+      }
+
+      getTodoForm() : DebugElement {
+        return this.fixture.debugElement.query(By.css('#todo-form'));
+      }
+
+      submitForm(form: DebugElement) {
+        form.triggerEventHandler('submit', null);
+
+        this.fixture.detectChanges();
+        tick();
+      }
+    }
 });
